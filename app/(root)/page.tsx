0a866750d7482769ac92a53323e3ff455d22cf4b
Emmanuel Nkiriyumwami <nkiliyumwami@gmail.com>
2023-07-06 20:32:49 -0400

    form/vakidation with zod and react hook form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
-    <div className="flex item-center justify-center h-full">
-      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}>
-        Children
-      </Modal>
-    </div>
+    <div className="flex item-center justify-center h-full">Root Page</div>
   );
 };
 
